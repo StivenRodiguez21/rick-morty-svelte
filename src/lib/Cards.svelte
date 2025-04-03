4bce2b1027de4ea6b04d86a768ede1b6aef7c12b
StivenRodiguez21 <stivendavidrodriguezbarroso4@gmail.com>
2025-04-03 17:00:26 -0500

    iiii

---
+++ src/lib/Cards.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let caracter;
+</script>
+<div class="subcard">
+<img src={caracter.image} alt={caracter.name}/>
+<h2>{caracter.name}</h2>
+<h2>{caracter.species}</h2>
+<h2>{caracter.status}</h2>
+</div>
